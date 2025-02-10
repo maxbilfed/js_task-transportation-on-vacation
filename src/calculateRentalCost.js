35a82c2dc83c7f347b53d2df0eb795acb2cb4ded
Max Shapes <makc.bilous04@gmail.com>
2025-02-10 16:54:13 +0200

    made a solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,18 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const price = 40;
+  let total = days * price;
+
+  if (days >= 3) {
+    total -= 20;
+  }
+
+  if (days >= 7) {
+    total -= 30;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
