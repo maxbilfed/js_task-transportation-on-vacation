7338b26d97d20a23409b8052b76a210c0017a9ca
Max Shapes <makc.bilous04@gmail.com>
2025-02-10 23:05:52 +0200

    fixed logic

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,16 +6,18 @@
 function calculateRentalCost(days) {
   // write code here
   const price = 40;
-  let total = days * price;
+  const total = days * price;
+  const shortTerm = 3;
+  const longTerm = 7;
   const shortTermDiscount = 20;
   const longTermDiscount = 30;
 
-  if (days >= 3) {
-    total -= shortTermDiscount;
+  if (days >= shortTerm) {
+    return total - shortTermDiscount;
   }
 
-  if (days >= 7) {
-    total -= longTermDiscount;
+  if (days >= longTerm) {
+    return total - longTermDiscount;
   }
 
   return total;
